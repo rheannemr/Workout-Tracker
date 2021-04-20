451dcf02c0d06a9813a0c8f7076715b87f2b0d94
Rheanne Reyes <rheannemr@gmail.com>
2021-04-19 23:00:33 -0700

    Create route

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan")
+const apiRoutes = require("./routes/api")
 
 const PORT = process.env.PORT || 8080;
 
@@ -9,16 +10,15 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan("dev"));
-
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require("./routes/api"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
