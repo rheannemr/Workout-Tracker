b0798f06accbdb32c2aa826e2ed1a149c6f47a16
Rheanne Reyes <rheannemr@gmail.com>
2021-04-19 22:57:37 -0700

    require and use morgan

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const morgan = require("morgan")
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(morgan("dev"));
 
 app.use(express.static("public"));
 
